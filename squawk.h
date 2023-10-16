dda05533c9e4daa5c58270f1bbf96b542fc9d0c7
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-10-17 02:05:15 +0330

    added: header declrations

--- squawk.h
+++ squawk.h
@@ -6,7 +6,9 @@
 
 typedef struct Symbol 		 symbol_t;
 typedef struct Squawk		 squawk_t;
+typedef struct Slhtbl		 slhtbl_t;
 typedef enum   DefaultVar	 dflvar_t;
+typedef enum   Symtype		 symtype_t;
 
 int execute_and_rw(uint8_t* id_stream, uint8_t *id_var, 
 				const uint8_t* command, 
@@ -15,11 +17,17 @@ int execute_and_rw(uint8_t* id_stream, uint8_t *id_var,
 				size_t *result_len,
 				bool close_after);
 
-static symbol_t* symtable_upsert(uint8_t *id, 
-				uintptr_t value, 
-				enum Symtype type,
-				bool redefine);
+static inline void sym_init(void);
 
+static void sym_put(uint8_t *id, 
+			uintptr_t value, 
+			symtype_t type);
+
+static symtype_t sym_get(uint8_t* id, uintptr_t *value);
+
+static void sym_remove(uint8_t* id);
+
+static inline void sym_resize(void);
 
 static inline uint8_t* gc_concat_str(uint8_t *string_1, 
 					uint8_t *string_2,
@@ -34,15 +42,6 @@ static void upsert_default_vars(void);
 void do_on_sigint(int signum);
 
 void do_on_exit(void);
-
-
-
-
-
-
-
-
-
 
 
 
